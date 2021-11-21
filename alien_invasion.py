864ac904fc2c13386f176f01395a7bf01ee98541
Naveen Kollu <naveenkollu10@gmail.com>
2021-11-21 19:09:52 +0530

    Initial commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ from bullet import Bullet
 from alien import Alien
 from button import Button
 
-class GalaxyGuardians:
+class AlienInvasion:
     """class to manage game assets and behavior."""
 
     def __init__(self):
@@ -258,5 +258,5 @@ class GalaxyGuardians:
 
 if __name__ == "__main__":
     # Makes a game instance and runs the game.
-    ai = GalaxyGuardians()
+    ai = AlienInvasion()
     ai.run_game()
