2f201e5a316693b6b4216f32ecf014eb9b5055f5
Naveen Kollu <naveenkollu10@gmail.com>
2021-11-21 13:10:03 +0530

    Initial commit

--- scoreboard.py
+++ scoreboard.py
@@ -66,7 +66,7 @@ class Scoreboard:
         level_str = str(self.stats.level)
         self.level_image = self.font.render(level_str, True,
                 self.text_color, self.settings.bg_color)
-    
+
         # Position the level below zero.
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
